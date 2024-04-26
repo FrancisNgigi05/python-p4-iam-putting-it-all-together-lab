093624035e746ea93a1cb1b6e00db2668e21d65c
toxicanonymous253 <ngugifrancis930@gmail.com>
2024-04-26 14:19:49 +0300

    The models.py file has been documented and is a success

--- server/models.py
+++ server/models.py
@@ -3,48 +3,47 @@ from sqlalchemy_serializer import SerializerMixin
 
 from config import db, bcrypt
 
+# The users table for the db
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    serialize_rules = ('-recipes.user', '-_password_hash',)
-
     id = db.Column(db.Integer, primary_key=True)
+
     username = db.Column(db.String, unique=True, nullable=False)
-    _password_hash = db.Column(db.String)
+    _password_hash = db.Column(db.String, nullable=False)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
+    """Time to initialize relationship"""
+    recipes = db.relationship('Recipe', back_populates="user", cascade='all, delete-orphan') # Assigning users their recepies    
 
-    recipes = db.relationship('Recipe', backref='user')
-
+    def __repr__(self):
+        return f'<User {self.username}, ID {self.id}>'
+    
+    """Time to make this password to be stored securely to
+    avoid sensitive data to be leaked"""
     @hybrid_property
     def password_hash(self):
-        raise AttributeError('Password hashes may not be viewed.')
-
+        return self._password_hash
+    
     @password_hash.setter
     def password_hash(self, password):
-        password_hash = bcrypt.generate_password_hash(
-            password.encode('utf-8'))
+        password_hash = bcrypt.generate_password_hash(password.encode('utf-8'))
+
         self._password_hash = password_hash.decode('utf-8')
 
     def authenticate(self, password):
-        return bcrypt.check_password_hash(
-            self._password_hash, password.encode('utf-8'))
+        return bcrypt.check_password_hash(self._password_hash, password.endcode('utf-8'))
 
-    def __repr__(self):
-        return f'<User {self.username}>'
-
+# The recepie table for the db
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    __table_args__ = (
-        db.CheckConstraint('length(instructions) >= 50'),
-    )
 
     id = db.Column(db.Integer, primary_key=True)
+
     title = db.Column(db.String, nullable=False)
-    instructions = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, db.CheckConstraint('len(instructions) >= 50'))
     minutes_to_complete = db.Column(db.Integer)
-
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
-
-    def __repr__(self):
-        return f'<Recipe {self.id}: {self.title}>'
+    """Adding a foerign key to connect the user table to the recpie table"""
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id')) # Connection gained
+    """Acquriring relationship between the two tables which are users and recipes"""
+    user = db.relationship('User', back_populates="recipes") # Assigning a user to their recpective recepies
